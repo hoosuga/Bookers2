bd39ca16333e783c4f5b5d0ae631661ea355ed8d
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-03 04:48:00 +0000

    [Add] 4-27 登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,22 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
   def show
   end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
