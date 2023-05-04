772abf139d326f1fd53f0c8b8f54a675002512eb
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-04 11:30:46 +0000

    [Add] 4-27 フラッシュメッセージの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -13,12 +25,14 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    @user_id = current_user.id
   end
 
   def update
     @user = User.find(params[:id])
     @user = current_user
     if @user.update(user_params)
+      flash[:notice] ="You have updated user successfully."
       redirect_to user_path(@user.id)
     else
       @user = User.find(params[:id])
@@ -27,6 +41,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
