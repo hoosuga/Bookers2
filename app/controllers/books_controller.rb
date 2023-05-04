93b2a5b7e63753f013ba185eb0408c92bf12a52c
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-05-04 10:02:06 +0000

    [Add] 4-27 Bookモデルにバリデーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,23 +8,32 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
-    @user =current_user
+    @newbook = Book.new
   end
 
   def edit
     @book = Book.find(params[:id])
+    @book.user_id = current_user.id
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    @book = Book.new
+    @newbook = Book.new
     @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @newbook = Book.new
   end
 
   def edit
@@ -20,6 +21,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       redirect_to user_path(@user.id)
     else
+      @user = User.find(params[:id])
       render :edit
     end
   end
